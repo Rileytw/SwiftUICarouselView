c1ca0ffbbea5ab58d04f0148e448771065aec595
Riley <ring8255@gmail.com>
2025-07-29 18:53:07 +0800

    Add example app.

---
+++ Example/CarousalExampleApp/CarousalExampleApp/CarousalExampleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarousalExampleAppApp.swift
+//  CarousalExampleApp
+//
+//  Created by Lei on 2025/7/29.
+//
+
+import SwiftUI
+
+@main
+struct CarousalExampleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Example/CarousalExampleApp/CarousalExampleAppTests/CarousalExampleAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  CarousalExampleAppTests.swift
+//  CarousalExampleAppTests
+//
+//  Created by Lei on 2025/7/29.
+//
+
+import Testing
+@testable import CarousalExampleApp
+
+struct CarousalExampleAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
