a284b083af666d8f9420feefb33ec5880e5999e3
Riley <ring8255@gmail.com>
2025-07-29 18:20:20 +0800

    Update platforms for Package.

--- Package.swift
+++ Package.swift
@@ -5,6 +5,10 @@ import PackageDescription
 
 let package = Package(
     name: "SwiftUICarousalView",
+    platforms: [
+        .iOS(.v14),
+        .watchOS(.v7)
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
